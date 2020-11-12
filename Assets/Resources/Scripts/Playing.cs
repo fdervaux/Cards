116f365c459b107fcca2304d606d889e656fbfaa
Dervaux François <francois.dervaux@gmail.com>
2020-11-12 10:13:48 +0100

    update playing 12 13 14 15

--- Assets/Resources/Scripts/Playing.cs
+++ Assets/Resources/Scripts/Playing.cs
@@ -10,9 +10,23 @@ public class Playing : MonoBehaviour
     public Deck deck4;
     private GameManager gm = null;
     
+
+    public bool DeckIsNotEmpty(Deck deck)
+    {
+        return !gm.DeckIsEmpty(deck);
+    }
+
     public void ViderDeck(Deck deckDepart, Deck deckDArriver)
     {
         while (!gm.DeckIsEmpty(deckDepart))
+        {
+            gm.MoveTopCard(deckDepart, deckDArriver);
+        }
+    }
+
+    public void ViderCouleur(Deck deckDepart, Deck deckDArriver, CardColor couleur)
+    {
+        while (DeckIsNotEmpty(deckDepart) && gm.topCardColor(deckDepart) == couleur)
         {
             gm.MoveTopCard(deckDepart, deckDArriver);
         }
@@ -167,6 +181,93 @@ public class Playing : MonoBehaviour
         //"[T+P][K+C]" "" "" ""
     }
 
+    public void exerciceA12()
+    {
+        gm.InitDeck(deck1, "[K]");
+        gm.InitDeck(deck2, "[T]");
+
+        // SItuation finale "[K]" "" "[KT]" "" 
+        //                  "" "[T]" "[KT]" ""
+
+        // Pseudo-code:
+        // ***************************
+        // tant que deck1 n'est pas vide et deck2 n'est pas vide
+        // deplacer carte de deck1 sur deck3 
+        // puis deplacer carte deck2 sur deck3
+
+        while (DeckIsNotEmpty(deck1) && DeckIsNotEmpty(deck2) )
+        {
+            gm.MoveTopCard(deck1, deck3);
+            gm.MoveTopCard(deck2, deck3);
+        }
+    }
+
+    public void exerciceA13()
+    {
+        // Pseudo-code:
+        // *****************
+        // tant que j'ai des cartes sur le deck1 
+        //      Je déplace la carte du sommet du deck1 sur le deck2 "On vérifie avec la condition du while qu'il y a au moins une carte sur le deck" 
+        //      Je déplace la carte du sommet du deck1 sur le deck3 => impossible si le deck1 est vide => Erreur "mais on ne vérifie pas qu'il y ai deux carte"
+
+        // Le bonne algorithme:
+        // tant que j'ai des cartes sur le deck1 
+        //      Je déplace la carte du sommet du deck1 sur le deck2
+        //      si le deck contient encore une carte
+        //          Je déplace la carte du sommet du deck1 sur le deck3 
+
+        gm.InitDeck(deck1, "[T]");
+
+        while (DeckIsNotEmpty(deck1))
+        {
+            gm.MoveTopCard(deck1, deck2);
+
+            if(DeckIsNotEmpty(deck1))
+            {
+                gm.MoveTopCard(deck1, deck3);
+            }
+        }
+    }
+
+
+    public void exerciceA14()
+    {
+        gm.InitDeck(deck1, "[T]");
+        gm.InitDeck(deck2, "[K]");
+        gm.InitDeck(deck3, "[P]");
+
+        ViderDeck(deck3, deck4);
+        ViderDeck(deck2, deck3);
+        ViderDeck(deck1, deck2);
+    }
+
+
+    public void exerciceA15()
+    {
+        gm.InitDeck(deck1, "[T]T");
+        gm.InitDeck(deck2, "[K]K");
+        gm.InitDeck(deck3, "[C]C");
+        gm.InitDeck(deck4, "[P]P");
+
+        /*
+            Situation finale :
+            Deck 1 : "[P]"
+            Deck 2 : "[T]"
+            Deck 3 : "[K]"
+            Deck 4 : "[C]"
+        */
+
+        ViderDeck(deck4, deck1);
+        ViderDeck(deck3, deck4);
+        // Deck 1 : "[T][P]" Deck 2 : "[K]" Deck 3 : "" Deck 4 : "[C]"
+        ViderDeck(deck2, deck3);
+        // Deck 1 : "[T][P]" Deck 2 : "" Deck 3 : "[K]" Deck 4 : "[C]"
+        ViderCouleur(deck1, deck3, CardColor.Pique);
+        // Deck 1 : "[T]" Deck 2 : "" Deck 3 : "[K][P]" Deck 4 : "[C]"
+        ViderDeck(deck1, deck2);
+        // Deck 1 : "" Deck 2 : "[T]" Deck 3 : "[K][P]" Deck 4 : "[C]"
+        ViderCouleur(deck3, deck1, CardColor.Pique);
+    }
 
     private void Start()
     {
@@ -176,7 +277,7 @@ public class Playing : MonoBehaviour
         // *********** !!! Your code here !!! ***********
         // **********************************************
 
-        exerciceA11();
+        exerciceA15();
 
         // **********************************************
         // **********************************************
