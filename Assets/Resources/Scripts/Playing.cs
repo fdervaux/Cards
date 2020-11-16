a15c801ff30c6f6d714d341686007bed2f325b74
Dervaux François <francois.dervaux@gmail.com>
2020-11-16 08:15:15 +0100

    add Correction

--- Assets/Resources/Scripts/Playing.cs
+++ Assets/Resources/Scripts/Playing.cs
@@ -8,16 +8,155 @@ public class Playing : MonoBehaviour
     public Deck deck2;
     public Deck deck3;
     public Deck deck4;
+    private GameManager gm = null;
+    private List<int> fibonacciResults = new List<int>();
 
-    public GameManager gm;
+    public void viderCouleur(Deck from, Deck to, CardColor color)
+    {
+        while (!gm.DeckIsEmpty(from) && gm.topCardColor(from) == color)
+        {
+            gm.MoveTopCard(from, to);
+        }
+    }
 
-    private void Start() {
+    public void ViderDeck(Deck from, Deck to)
+    {
+        while (!gm.DeckIsEmpty(from))
+        {
+            gm.MoveTopCard(from, to);
+        }
+    }
 
+    public void trouverPlusPetit(Deck depart, Deck deckPlusPetit, Deck reste)
+    {
+        if (!gm.DeckIsEmpty(depart))
+        {
+            gm.MoveTopCard(depart, deckPlusPetit);
+        }
+
+        while (!gm.DeckIsEmpty(depart))
+        {
+            if (gm.Superior(depart, deckPlusPetit))
+            {
+                gm.MoveTopCard(depart, reste);
+            }
+            else
+            {
+                gm.MoveTopCard(deckPlusPetit, reste);
+                gm.MoveTopCard(depart, deckPlusPetit);
+            }
+        }
+    }
+
+    public void trierDeckPlusPetitAuPlusGrand(Deck depart, Deck Arrivée, Deck Transitoire)
+    {
+        while (!gm.DeckIsEmpty(depart))
+        {
+            trouverPlusPetit(depart, Arrivée, Transitoire);
+            trouverPlusPetit(Transitoire, Arrivée, depart);
+        }
+    }
+
+    public void exerciceTest()
+    {
+        gm.InitDeck(deck1, "TTTCCC");
+        viderCouleur(deck1, deck3, CardColor.Coeur);
+        viderCouleur(deck1, deck2, CardColor.Treffle);
+    }
+
+    public void exercice2()
+    {
+        gm.InitDeck(deck1, "TTTTTT");
+        ViderDeck(deck1, deck2);
+        trierDeckPlusPetitAuPlusGrand(deck2, deck1, deck3);
+
+    }
+
+    public int fibonacci(int n)
+    {
+        int resultat = 0;
+        Debug.Log("appel " + n);
+
+        if (n == 0)
+        {
+            resultat = 0;
+        }
+        else if (n == 1)
+        {
+            resultat = 1;
+        }
+        else
+        {
+            resultat = fibonacci(n - 1) + fibonacci(n - 2);
+        }
+
+        return resultat;
+    }
+
+    public int fibonacciIntelligent(int n)
+    {
+        int resultat = 0;
+        Debug.Log("appel " + n);
+
+        if (n == 0)
+        {
+            resultat = 0;
+        }
+        else if (n == 1)
+        {
+            resultat = 1;
+        }
+        else
+        {
+            resultat = fibonacci(n - 1) + fibonacci(n - 2);
+        }
+
+        fibonacciResults.Insert(n, resultat);
+        return resultat;
+    }
+
+
+
+    private void Start()
+    {
         gm = GameManager.Instance();
 
         // **********************************************
         // *********** !!! Your code here !!! ***********
         // **********************************************
+
+        exercice2();
+
+        //fibonacciResults.insert(0,1);
+        //fibonacciResults.Add(1);
+        //fibonacciResults.Add(1);
+        //fibonacciResults.Add(2);
+
+        /*Debug.Log(fibonacciResults[0]);
+        Debug.Log(fibonacciResults[1]);
+        Debug.Log(fibonacciResults[2]);
+        Debug.Log(fibonacciResults[3]);
+        Debug.Log(fibonacciResults[4]);
+        */
+
+        //Debug.Log(fibonacci(3));
+
+
+
+        //Suite de fibonacci
+        // Un+1 = Un + Un-1
+        // U0 = 0 U1 = 1
+
+        // U2 = U1 + U0 = 1 + 0 = 1
+        // U3 = U2 + U1 = 1 + 1 = 2
+        // U4 = 3
+        // U5 = 5
+        // U6 = 8
+        // U7 = 13
+        // ....
+
+        // impératif (suite d'instruction dans une fonction)
+        // recursif (appel de la fonction dans la fonction)
 
         // **********************************************
         // **********************************************
