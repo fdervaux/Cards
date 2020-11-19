d38175f1b9afb1993ba02332ddb08c9c13d8028d
Dervaux François <francois.dervaux@gmail.com>
2020-11-19 12:03:06 +0100

    update

--- Assets/Resources/Scripts/Cours.cs
+++ Assets/Resources/Scripts/Cours.cs
@@ -18,14 +18,34 @@ public class Cours : MonoBehaviour
         return res;
     }
 
+
+    // V0 = 4
+    // Vn = Vn-1 * 3 
+    public int V(int n)
+    {
+        int res = 4;
+
+        for (int i = 1; i < n + 1; i++)
+        {
+            res = res * 3;
+        }
+
+        return res;
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hello world");
 
 
-        // V0 = 4
-        // Vn = Vn-1 * 3 
+        // UU0 = 1, UUn = 5 + (UUn-2 * 3)
+
+        // fibonacci
+        // F0 = 0 , F1 = 1, Fn = Fn-1 + Fn-2
+
+
 
 
 
@@ -44,6 +64,12 @@ public class Cours : MonoBehaviour
         Debug.Log("U(3) = " + U(3));
         Debug.Log("U(4) = " + U(4));
         Debug.Log("U(5) = " + U(5));
+
+
+        for (int i = 0; i <= 15; i++)
+        {
+            Debug.Log("V("+ i +") = " + V(i));
+        }
 
 
     }
